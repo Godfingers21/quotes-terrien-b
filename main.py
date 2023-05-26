47827039d495a1a39c210e69d4a821d103b66be5
Eloiroy <125263912+Eloiroy@users.noreply.github.com>
2023-05-26 09:17:48 +0200

    Update main.py
    update ou conséquence

--- main.py
+++ main.py
@@ -13,19 +13,18 @@ def main():
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-3): ")
+        choice = input(">> ")
         
-        if choice == "1":
+        if choice == "random":
             print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
-        elif choice == "3":
-            add_quote(quotes, quote.txt)
-        elif choice == "4":
+        elif choice == "display":
             count = int(input("Enter the number of quotes to display: "))
             display_quotes(quotes, count)
-        elif choice == 5:
-            print("Goodbye...")
+        elif choice == "add":
+            add_quote(quotes, "quotes.txt")
+        elif choice == "exit":
+            print("Good bye...")
+            break
         else:
             print("Invalid input")
 
